3fafbe6df02703ae1052609a8fcba59de1225348
jsfc <jsfc2199@gmail.com>
2022-01-05 21:49:06 -0500

    se sube el proyecto

---
+++ PreguntasYRespuestas.py
@@ -0,0 +1,238 @@
+lvl1={"Arte y Cine":"¿Quien es Tom Holland?",
+      "Matematicas":"¿Cuanto es Pi?",
+      "Deportes":"¿Cuanto dura un partido de fútbol?",
+      "Musica":"¿Que música toca Metallica?",
+      "Ciencia":"¿Cuantas patas tiene una araña?"
+      }
+    
+lvl2={"Arte y Cine":"¿Quien pintó la Mona Lisa?",
+      "Matematicas":"¿De qué fórmula es la expresión A = π*r^2?",
+      "Deportes":"¿Qué país fue campeón del mundial de fútbol del 2018?",
+      "Musica":"¿Cual es la primera nota en la música?",
+      "Ciencia":"¿Cuantos huesos tiene un cuerpo humano adulto?"
+      }
+
+lvl3={"Arte y Cine":"¿Cuantos libros tiene la novela Harry Potter?",
+      "Matematicas":"¿Que es e=mc^2?",
+      "Deportes":"¿Cuantos balones de oro tiene Mbappe?",
+      "Musica":"¿Cuantas notas hay en la música?",
+      "Ciencia":"¿Qué parte del cuerpo tiene 27 huesos y 35 músculos?"
+      }
+
+lvl4={"Arte y Cine":"¿Quién pintó el Jardín de las Delicias?",
+      "Matematicas":"¿Si la velocidad es constante ¿qué pasa con la aceleración?",
+      "Deportes":"¿Quién fue el primer campeón de la cahmpions league?",
+      "Musica":"¿Que es # en la música?",
+      "Ciencia":"¿Cuantas lunas tiene Jupiter?"
+      }
+
+lvl5={"Arte y Cine":"¿De que nacionalidad era Heri Cartier Bresson?",
+      "Matematicas":"¿Como se llama un poliedro de 20 caras?",
+      "Deportes":"¿Cuando fue el primer mundial de futbol? (año)",
+      "Musica":"¿Que instrumento usa clave de sol y de fa?",
+      "Ciencia":"¿Que enfermedad se caracteriza por repentinos ataques de sueño?"
+      }
+
+preguntas = [lvl1,lvl2,lvl3,lvl4,lvl5]
+ 
+respuesta1 = {"Arte y Cine":"A", 
+              "Matematicas":"C",
+              "Deportes":"A",
+              "Musica":"C",
+              "Ciencia":"D",                              
+              }
+respuesta2 = {"Arte y Cine":"B", 
+              "Matematicas":"B",
+              "Deportes":"D",
+              "Musica":"A",
+              "Ciencia":"B",                              
+              }
+respuesta3 = {"Arte y Cine":"A", 
+              "Matematicas":"B",
+              "Deportes":"B",
+              "Musica":"C",
+              "Ciencia":"B",                              
+              }
+respuesta4 = {"Arte y Cine":"A", 
+              "Matematicas":"D",
+              "Deportes":"B",
+              "Musica":"C",
+              "Ciencia":"D",                              
+              }
+respuesta5 ={"Arte y Cine":"A", 
+              "Matematicas":"A",
+              "Deportes":"B",
+              "Musica":"C",
+              "Ciencia":"B",                              
+              }
+
+respuestas=[respuesta1,respuesta2,respuesta3,respuesta4,respuesta5]
+
+opcionesLvl1={"Arte y Cine":{
+                                "A":"Actor",
+                                "B":"Futbolista",
+                                "C":"Musico",
+                                "D":"Pintor"
+                            },
+    
+              "Matematicas":{
+                                "A":"3.2",
+                                "B":"2.25",
+                                "C":"3.14",
+                                "D":"2.56"
+                                },
+              "Deportes":{
+                                "A":"90 minutos",
+                                "B":"60 minutos",
+                                "C":"85 minutos",
+                                "D":"30 minutos"
+                                },
+              "Musica":{
+                                "A":"Rock",
+                                "B":"Jazz",
+                                "C":"Metal",
+                                "D":"Rap"
+                                },
+              "Ciencia":{
+                                "A":"5",
+                                "B":"9",
+                                "C":"12",
+                                "D":"8"
+                                }
+      }
+
+opcionesLvl2={"Arte y Cine":{
+                                "A":"Salvador Dalí",
+                                "B":"Di Caprio",
+                                "C":"Picasso",
+                                "D":"Van Gogh"
+                            },
+    
+              "Matematicas":{
+                                "A":"Área esfera",
+                                "B":"Área círculo",
+                                "C":"Área cuadrado",
+                                "D":"Área Cono"
+                                },
+              "Deportes":{
+                                "A":"Alemania",
+                                "B":"España",
+                                "C":"Portugal",
+                                "D":"Francia"
+                                },
+              "Musica":{
+                                "A":"Do",
+                                "B":"Fa",
+                                "C":"Sol",
+                                "D":"Mi"
+                                },
+              "Ciencia":{
+                                "A":"225",
+                                "B":"206",
+                                "C":"217",
+                                "D":"220"
+                                }
+      }
+
+opcionesLvl3={"Arte y Cine":{
+                                "A":"7",
+                                "B":"8",
+                                "C":"9",
+                                "D":"10"
+                            },
+    
+              "Matematicas":{
+                                "A":"Equivalencia entre masa y velocidad",
+                                "B":"Equivalencia entre energía y masa",
+                                "C":"Equivalencia entre energía y velocidad",
+                                "D":"Ninguna de las anteriores"
+                                },
+              "Deportes":{
+                                "A":"2",
+                                "B":"0",
+                                "C":"1",
+                                "D":"3"
+                                },
+              "Musica":{
+                                "A":"24",
+                                "B":"8",
+                                "C":"12",
+                                "D":"10"
+                                },
+              "Ciencia":{
+                                "A":"Pie",
+                                "B":"Mano",
+                                "C":"Brazo",
+                                "D":"Abdomen"
+                                }
+      }
+
+opcionesLvl4={"Arte y Cine":{
+                                "A":"El Bosco",
+                                "B":"Carvaggio",
+                                "C":"Velasquez",
+                                "D":"Arcimbolo"
+                            },
+    
+              "Matematicas":{
+                                "A":"Constante",
+                                "B":"Decreciente",
+                                "C":"Creciente",
+                                "D":"Nula"
+                                },
+              "Deportes":{
+                                "A":"Barcelona",
+                                "B":"Real Madrid",
+                                "C":"Bayern Munich",
+                                "D":"Nottingham Forest"
+                                },
+              "Musica":{
+                                "A":"Tono arriba",
+                                "B":"Dos tonos arriba",
+                                "C":"Medio Tono arriba",
+                                "D":"Dos tonos y medio arriba"
+                                },
+              "Ciencia":{
+                                "A":"80",
+                                "B":"78",
+                                "C":"77",
+                                "D":"79"
+                                }
+      }
+
+opcionesLvl5={"Arte y Cine":{
+    
+
+                                "A":"Francesa",
+                                "B":"Alemana",
+                                "C":"Escocesa",
+                                "D":"Turca"
+                            },
+    
+              "Matematicas":{
+                                "A":"Icosaedro",
+                                "B":"Dodecaedro",
+                                "C":"Tetracaedro",
+                                "D":"Apeirocaedro"
+                                },
+              "Deportes":{
+                                "A":"1934",
+                                "B":"1930",
+                                "C":"1926",
+                                "D":"1938"
+                                },
+              "Musica":{
+                                "A":"Guitarra",
+                                "B":"Bajo",
+                                "C":"Piano",
+                                "D":"Banjo"
+                                },
+              "Ciencia":{
+                                "A":"Bruxismo",
+                                "B":"Narcolepsia",
+                                "C":"Parasomnias",
+                                "D":"Sonambulismo"
+                                }
+      }
+
+opciones = [opcionesLvl1, opcionesLvl2, opcionesLvl3, opcionesLvl4, opcionesLvl5]
